bb53dbbd82536898a7b56be753e3c55b32e13d7d
Mohamed Zein <mzeinhu@gmail.com>
2021-08-12 11:33:09 -0700

    ETL done

--- parser.js
+++ parser.js
@@ -17,35 +17,33 @@ fs.createReadStream('./CSV/questions.csv')
   .pipe(parse({ columns: true }))
   .on('data', (data) => {
     // questionsOutput.push(data)
-    if (data.id > 3518864) {
-      const {
-        product_id,
-        body,
-        date_written,
-        asker_name,
-        asker_email,
-        reported,
-        helpful,
-      } = data;
+    const {
+      product_id,
+      body,
+      date_written,
+      asker_name,
+      asker_email,
+      reported,
+      helpful,
+    } = data;
 
-      const q = 'Insert into questions (product_id, question_body, question_date, asker_name, asker_email, question_helpfulness, reported) VALUES ($1, $2, to_timestamp($3), $4, $5, $6, $7)';
-      const values = [
-        product_id,
-        body,
-        date_written/1000,
-        asker_name,
-        asker_email,
-        helpful,
-        reported,
-      ];
-      db.query(q, values, (err, res) => {
-        if (err) {
-          console.log(err.stack);
-        } else {
-          // console.log(res.row[0]);
-        }
-      });
-    }
+    const q = 'Insert into questions (product_id, question_body, question_date, asker_name, asker_email, question_helpfulness, reported) VALUES ($1, $2, to_timestamp($3), $4, $5, $6, $7)';
+    const values = [
+      product_id,
+      body,
+      date_written/1000,
+      asker_name,
+      asker_email,
+      helpful,
+      reported,
+    ];
+    db.query(q, values, (err, res) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        // console.log(res.row[0]);
+      }
+    });
   })
   .on('end', () => {
     console.log('questionsOutput done');
@@ -57,35 +55,33 @@ fs.createReadStream('./CSV/answers.csv')
   .pipe(parse({ columns: true }))
   .on('data', (data) => {
     // id,question_id,body,date_written,answerer_name,answerer_email,reported,helpful
-    if (data.id > 4572147) {
-      const {
-        question_id,
-        body,
-        date_written,
-        answerer_name,
-        answerer_email,
-        reported,
-        helpful,
-      } = data;
+    const {
+      question_id,
+      body,
+      date_written,
+      answerer_name,
+      answerer_email,
+      reported,
+      helpful,
+    } = data;
 
-      const q = 'Insert into answers (question, body, date, answerer_name, answerer_email, helpfulness, reported) VALUES ($1, $2, to_timestamp($3), $4, $5, $6, $7)';
-      const values = [
-        question_id,
-        body,
-        date_written/1000,
-        answerer_name,
-        answerer_email,
-        helpful,
-        reported,
-      ];
-      db.query(q, values, (err, res) => {
-        if (err) {
-          console.log(err.stack);
-        } else {
-          // console.log(res.row[0]);
-        }
-      });
-    }
+    const q = 'Insert into answers (question, body, date, answerer_name, answerer_email, helpfulness, reported) VALUES ($1, $2, to_timestamp($3), $4, $5, $6, $7)';
+    const values = [
+      question_id,
+      body,
+      date_written/1000,
+      answerer_name,
+      answerer_email,
+      helpful,
+      reported,
+    ];
+    db.query(q, values, (err, res) => {
+      if (err) {
+        console.log(err.stack);
+      } else {
+        // console.log(res.row[0]);
+      }
+    });
   })
   .on('end', () => {
     console.log('answersOutput done');
